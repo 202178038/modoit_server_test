ede4d0c4bc99964f93d8d2722f143c6c847b2d7d
202178038 <202178038@yiu.ac.kr>
2025-07-16 17:47:47 +0900

    feat: cors 설정 변경 ! (이건 앱에서는 상관은 없을 듯?)

--- src/main/java/project/self_development/routine/configuration/WebConfig.java
+++ src/main/java/project/self_development/routine/configuration/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")  // 모든 출처 허용
+                .allowedOriginPatterns("*")  // 모든 출처 허용
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true);
