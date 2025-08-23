308282c72de3b03d56f648756b93941eb72846e4
202178038 <202178038@yiu.ac.kr>
2025-08-24 00:32:44 +0900

    feat: 시간대 한국 기준으로 수정

--- src/main/java/project/self_development/routine/SelfDevelopmentRoutineApplication.java
+++ src/main/java/project/self_development/routine/SelfDevelopmentRoutineApplication.java
@@ -3,11 +3,19 @@ package project.self_development.routine;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import jakarta.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class SelfDevelopmentRoutineApplication {
+
+	// ✅ 애플리케이션 전역 기본 타임존을 KST로 고정
+	@PostConstruct
+	public void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SelfDevelopmentRoutineApplication.class, args);
 	}
-
 }
